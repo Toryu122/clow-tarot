ec235bc2939b6ba1d8139116c91f7f0829558621
thuan122 <127835141+thuan122@users.noreply.github.com>
2023-04-11 14:24:09 +0700

    main page

--- src/index.js
+++ src/index.js
@@ -11,16 +11,13 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
